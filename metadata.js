60aba18470e1ff01c52e586368114834df14390b
Deadkeyz <151540063+Deadkeyz@users.noreply.github.com>
2024-07-16 00:56:29 -0400

    Update metadata.js

--- metadata.js
+++ metadata.js
@@ -1,7 +1,7 @@
 {
   "name": "Red Hawk",
   "symbol": "$HAWK",
-  "image": "https://raw.githubusercontent.com/Deadkeyz/redhawk/main/logo.png",
+  "image": "https://raw.githubusercontent.com/Deadkeyz/redhawk/main/logo.jpg",
   "description": "Hawk touch the Moon everyday",
   "website": "https://Redhawk.ichi",
   "twitter": "https://x.com/Redhawk",
