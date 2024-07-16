e753a879d257b77e2565fe0e5c94c38081dff618
Deadkeyz <151540063+Deadkeyz@users.noreply.github.com>
2024-07-16 00:17:00 -0400

    Create metadata.js

---
+++ metadata.js
@@ -0,0 +1,9 @@
+{
+  "name": "Red Hawk",
+  "symbol": "$HAWK",
+  "image": "https://raw.githubusercontent.com/Deadkeyz/redhawk/main/logo.jpg",
+  "description": "Hawk touch the Moon everyday",
+  "website": "https://Redhawk.ichi",
+  "twitter": "https://x.com/Redhawk",
+  "telegram": "https://t.me/Redhawk",
+}
